687fcdbe0c8714cb808681eff09b3f1e82103661
Sriram Rao <sriramrao@dhcp-10-8-019-233.mobile.reshsg.uci.edu>
2021-03-21 01:29:07 -0700

    Some code cleanup

--- rush-worker/domain/pipeline_manager.py
+++ rush-worker/domain/pipeline_manager.py
@@ -8,7 +8,7 @@ from repository.pipeline_repo import PipelineRepo
 
 
 class PipelineManager:
-    repository = PipelineRepo().get_instance()
+    repository = PipelineRepo.get_instance()
     __instance = None
     pipelines = {}
 
@@ -38,11 +38,17 @@ class PipelineManager:
         # add scheduled jobs to trigger job1 -> job2
         try:
             PipelineManager.repository.begin_transaction()
+            self.logger.info("Processing complete jobs")
             self.process_complete_jobs()
+            self.logger.info("Failing timed out jobs")
             self.process_timed_out_jobs()
+            self.logger.info("Processing failed jobs")
             self.process_failed_jobs()
+            self.logger.info("Triggering run requests")
             self.process_run_requests()
+            self.logger.info("Checking waiting jobs")
             self.process_waiting_jobs()
+            self.logger.info("Assigning ready jobs to free workers")
             self.assign_jobs_to_workers()
             PipelineManager.repository.commit_transaction()
         except Exception as e:
@@ -50,11 +56,9 @@ class PipelineManager:
             PipelineManager.repository.rollback_transaction()
 
     def process_waiting_jobs(self):
-        # for all waiting jobs if the ready time < current time, update status to ready
         PipelineManager.repository.process_waiting_jobs()
 
     def process_complete_jobs(self):
-        # for all complete jobs, move them to archive and (can be done later: ) trigger child jobs
         complete_jobs = PipelineManager.repository.get_runs_by_status(Status.COMPLETE)
         new_runs = []
         for run in complete_jobs:
@@ -73,7 +77,6 @@ class PipelineManager:
             raise Exception('New runs failed')
 
     def process_timed_out_jobs(self):
-        # for all running jobs, check if they are timed out. If they are, mark them failed
         running_jobs = PipelineManager.repository.get_runs_by_status(Status.RUNNING)
         timed_out_jobs = []
         for running_job in running_jobs:
@@ -86,8 +89,6 @@ class PipelineManager:
             raise Exception("Timed out jobs failed")
 
     def process_failed_jobs(self):
-        # for all failed jobs, add them in archive and
-        # add a waiting entry to be ready after the configured retry interval
         failed_jobs = PipelineManager.repository.get_runs_by_status(Status.FAILED)
         new_runs = []
         for failed_job in failed_jobs:
@@ -100,7 +101,6 @@ class PipelineManager:
             raise Exception("Insert runs failed")
 
     def process_run_requests(self):
-        # check all pending trigger requests and add them as ready jobs in running state
         requests = PipelineManager.repository.get_run_requests()
         new_runs = [request.get_run_state() for request in requests]
         if not PipelineManager.repository.insert_runs(new_runs) \
@@ -108,7 +108,6 @@ class PipelineManager:
             raise Exception("Error in run requests")
 
     def assign_jobs_to_workers(self):
-        # get the list of ready jobs and the list of available workers and assign one-to-one
         ready_jobs = PipelineManager.repository.get_runs_by_status(Status.READY)
         free_workers = PipelineManager.repository.get_workers_by_status('Free')
         assigned_states = []
@@ -158,6 +157,3 @@ class PipelineManager:
     def refresh():
         PipelineManager.pipelines = {pipeline.name: pipeline
                                      for pipeline in PipelineManager.repository.get_pipelines()}
-        # all_pipelines = PipelineManager.repository.get_pipelines()
-        # for pipeline in all_pipelines:
-        #     PipelineManager.pipelines[pipeline.name] = pipeline
--- rush-worker/main.py
+++ rush-worker/main.py
@@ -4,9 +4,7 @@ import configurator
 from scheduler import PeriodicScheduler
 
 if __name__ == '__main__':
-    # Set up worker
     worker_name = sys.argv[1] if len(sys.argv) > 1 else 'thread1'
     configurator.configure(worker_name)
-    # Call scheduler
     scheduler = PeriodicScheduler()
     scheduler.start()
