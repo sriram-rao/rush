0349107f361d7020646b65cb8f5d4828c4ac2ddb
Sriram Rao <sriram.rao04@gmail.com>
2025-06-28 20:13:24 -0700

    Cleaning up syntax

--- rush-worker/repository/sql_repo.py
+++ rush-worker/repository/sql_repo.py
@@ -1,61 +1,64 @@
-import string
-
 import psycopg2
+from psycopg2.extensions import cursor as Cursor, connection as Connection
 
 
 class SqlRepo:
     def __init__(self):
         # logger = logging.getLogger(current_thread().name)
-        self.connection = psycopg2.connect(
+        self.connection: Connection = psycopg2.connect(
             host="localhost",
             database="rushdb",
             user="sriramrao",
             password="")
-        self.cursor = None
+        self.active_cursor: Cursor | None = None
 
-    def fetch_entity(self, sql: string):
-        cursor = self.get_cursor()
-        cursor.execute(sql)
-        result = cursor.fetchone()
-        self.complete_cursor(cursor)
+    def fetch_entity(self, sql: str):
+        current_cursor = self.get_cursor()
+        current_cursor.execute(sql)
+        result = current_cursor.fetchone()
+        self.complete_cursor(current_cursor)
         return result
 
-    def fetch_entities(self, sql: string):
+    def fetch_entities(self, sql: str):
         cursor = self.get_cursor()
         cursor.execute(sql)
         result = cursor.fetchall()
         self.complete_cursor(cursor)
         return result
 
-    def execute(self, command: string) -> int:
+    def execute(self, command: str) -> int:
         cursor = self.get_cursor()
         cursor.execute(command)
         row_count = cursor.rowcount
-        if self.cursor is None:
+        if self.active_cursor is None:
             self.connection.commit()
         self.complete_cursor(cursor)
         return row_count
 
     def begin_transaction(self):
-        self.cursor = self.connection.cursor()
+        self.active_cursor = self.connection.cursor()
 
     def commit_transaction(self):
         self.connection.commit()
-        self.cursor.close()
-        self.cursor = None
+        if not self.active_cursor:
+            return
+        self.active_cursor.close()
+        self.active_cursor = None
 
     def rollback_transaction(self):
         self.connection.rollback()
-        self.cursor.close()
-        self.cursor = None
-
-    def get_cursor(self):
-        if self.cursor is None:
-            return self.connection.cursor()
-        return self.cursor
-
-    def complete_cursor(self, cursor):
-        if self.cursor is not None:
+        if not self.active_cursor:
             return
-        cursor.close()
+        self.active_cursor.close()
+        self.active_cursor = None
+
+    def get_cursor(self) -> Cursor:
+        if self.active_cursor is None:
+            return self.connection.cursor()
+        return self.active_cursor
+
+    def complete_cursor(self, active_cursor: psycopg2.extensions.cursor) -> None:
+        if self.active_cursor is not None:
+            return
+        active_cursor.close()
 
